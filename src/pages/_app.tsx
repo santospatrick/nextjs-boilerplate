74b2874b06039a01d6ac06a385aee2e7b53cb966
Patrick Santos <patrickspteixeira@gmail.com>
2021-03-08 11:29:52 -0300

    feat: add pwa support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content='"My trips" developed with Next.js/Typescript/GraphQL/Google Maps'
