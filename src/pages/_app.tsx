20bf587f0d202d1350656f7b2752f7a123f3565c
Patrick Santos <patrickspteixeira@gmail.com>
2021-03-08 11:51:40 -0300

    fix: update page title/description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>My Trips</title>
+        <title>Next.js</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content='"My trips" developed with Next.js/Typescript/GraphQL/Google Maps'
+          content="Next.js boilerplate with Typescript/Styled Components/Jest/Git Hooks"
         />
       </Head>
       <GlobalStyles />
