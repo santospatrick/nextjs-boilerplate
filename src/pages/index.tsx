7ff3652ef0f513b64e6b6eebd1dd75785a258465
Patrick Santos <patrickspteixeira@gmail.com>
2021-03-07 23:15:05 -0300

    style: add prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,13 +13,10 @@ function Home({ title = 'Next.js!' }: Props) {
       </Head>
 
       <main>
-        <h1>
-          {title}
-        </h1>
+        <h1>{title}</h1>
 
         <p>
-          Get started by editing{' '}
-          <code>pages/index.js</code>
+          Get started by editing <code>pages/index.js</code>
         </p>
 
         <div>
@@ -33,18 +30,12 @@ function Home({ title = 'Next.js!' }: Props) {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples">
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-
-          >
+          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -59,8 +50,7 @@ function Home({ title = 'Next.js!' }: Props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" />
         </a>
       </footer>
     </div>
