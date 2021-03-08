5ae0b045b060454a7c28bb7a96e40de807038e15
Patrick Santos <patrickspteixeira@gmail.com>
2021-03-08 00:26:06 -0300

    build: add rtl config

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,16 @@
+import { screen, render } from '@testing-library/react'
+import Main from './Main'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /react avançado/i,
+      })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
