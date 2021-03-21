1656b04719f118765e03c24dfe2977fecaf0ef02
Patrick Santos <patrickspteixeira@gmail.com>
2021-03-21 19:27:00 -0300

    test(jest): update files to get reports from

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 }
